99c5021857b50f5dea708673dd7e1d4bdad342fd
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2020-11-17 21:28:30 +0600

    fix a small error

--- burger-builder/src/components/Navigation/Toolbar/Toollbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toollbar.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import classes from './Toolbar.css'
-import Logo from '../../Logo/Logo'
-import NavigationItems from '../NavigationItems/NavigationItems';
+import React from "react";
+import classes from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <div>MANU</div>
-        <Logo/>
-        <nav>
-                <NavigationItems/>
-        </nav>
-    </header>
-)
+  <header className={classes.Toolbar}>
+    <div>MANU</div>
+    <Logo />
+    <nav>
+      <NavigationItems />
+    </nav>
+  </header>
+);
 
-export default toolbar
+export default toolbar;
