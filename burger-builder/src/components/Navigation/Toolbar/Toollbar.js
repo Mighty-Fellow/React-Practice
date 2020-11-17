2c5c82163d07fc10cf2d433f5a2e5d57eeecbdb0
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2020-11-17 19:11:55 +0600

    Using a Logo in our Application📑📑

--- burger-builder/src/components/Navigation/Toolbar/Toollbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toollbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import classes from './Toolbar.css'
+import Logo from '../../Logo/Logo'
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MANU</div>
-        <div>LOGO</div>
+        <Logo/>
         <nav>
             <ul>
 
