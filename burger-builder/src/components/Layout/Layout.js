b639e9089878f3207b84143bece84008ac114903
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2020-11-19 13:02:42 +0600

    Adding a Hamburger Icon

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -19,14 +19,16 @@ class Layout extends Component {
     render() {
         return (
             <Aux>
-            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
-            <SideDrawer open={this.state.sideDrawer} closed={this.sideDrawerClosedHandler}/>
-            <main className={classes.Content}>
-                {this.props.children}
-            </main>
-        </Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.sideDrawer} closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
         );
     }
 }
 
 export default Layout;
+
+
--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,28 +1,30 @@
-import React from "react";
+import React from 'react';
 
-import Logo from "../../Logo/Logo";
-import NavigationItems from "../NavigationItems/NavigationItems";
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 import Aux from '../../../hoc/Auxi'
-import Backdrop from '../../UI/Backdrop/Backdrop'
-import classes from "./SideDrawer.css";
-const sideDrawer = (props) => {
-  let attachedClass = [classes.SideDrawer,classes.Close]
-  if(props.open) {
-    attachedClass = [classes.SideDrawer,classes.Open]
-  }
-  return (
-    <Aux>
-      <Backdrop show={props.open} clicked={props.closed}/>
-      <div className={attachedClass.join(' ')}>
-        <div className={classes.Logo}>
-          <Logo/>
-        </div>
-        <nav>
-          <NavigationItems />
-        </nav>
-      </div>
-    </Aux>
-  );
+import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
+
+const sideDrawer = ( props ) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+    );
 };
 
 export default sideDrawer;
