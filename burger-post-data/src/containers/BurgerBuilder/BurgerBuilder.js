d54599fb230a88733623629a1b26ef053721ab3b
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2020-12-03 20:48:30 +0600

    Handling error part 2

--- burger-post-data/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-post-data/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -149,4 +149,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default withErrorHandler(BurgerBuilder);
+export default withErrorHandler(BurgerBuilder,axios);
--- burger-post-data/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-post-data/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { Component }from 'react';
 import Modal from '../../components/UI/Modal/Modal'
 
 import Aux from '../Auxi/Auxi';
 
-const withErrorHandler = ( WrappedComponent)=>{
-    return (props)=>{
-        return (
-            <Aux>
-                <Modal>
-                    Somethings didn't' work properly!
-                </Modal>
-                <WrappedComponent {...props}/>
-            </Aux>
-        )
+const withErrorHandler = ( WrappedComponent,axios)=>{
+     return class extends Component{
+         state={
+             error:null,
+         }
+         componentDidMount(){
+             axios.interceptors.request.use(req=>{
+                 this.setState({error:null})
+                 return req
+             })
+             axios.interceptors.response.use(req=>req , error=>{
+                this.setState({error:error})
+             })
+         }
+         errorConfirmedHandler(){
+             this.setState({error:null})
+         }
+        render(){
+            return(
+                <Aux>
+                    <Modal 
+                    show={this.state.error}
+                    modalClosed={this.errorConfirmedHandler}
+                    >
+                        {this.state.error ? this.state.error.message : null}
+                    </Modal>
+                    <WrappedComponent {...this.props}/>
+                </Aux>
+            )
+        }
     }
 }
 
