9218d11305b36e8b91b25226875a433b8e375dc0
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2020-12-13 12:11:34 +0600

    Switching Between Pages

--- http-ajax-blog/src/containers/Blog/Blog.js
+++ http-ajax-blog/src/containers/Blog/Blog.js
@@ -4,7 +4,9 @@ import { Route } from 'react-router-dom'
 // import axios from '../../axios'
 
 // import Post from '../../components/Post/Post';
+import NewPost from './NewPost/NewPost';
 import Posts from './Posts/Posts';
+
 import './Blog.css';
 class Blog extends Component {
     render() {
@@ -16,13 +18,14 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><a href="/"> Home</a></li>
-                            <li><a href="/new-post"> New Post</a></li>
+                            <li><a href="/new-posst"> New Post</a></li>
                         </ul>
                     </nav>
                 </header>
                {/*<Route path="/" exact render={() => <h1>Home</h1>}/>
                <Route path="/"  render={() => <h1>Home 2</h1>}/>*/}
                <Route path="/" exact component={Posts}/>
+               <Route path="/new-posst" component={NewPost}/>
             </div>
         );
     }
